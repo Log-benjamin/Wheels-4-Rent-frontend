921fd11748cef92a1d10f05f849746ab6365697b
Bettenoch <kipngenoemmanuel479@gmail.com>
2023-11-13 20:04:45 +0300

    fix the linters

--- src/component/Home/Login.js
+++ src/component/Home/Login.js
@@ -1,4 +1,3 @@
-/* eslint linebreak-style: ["error", "windows"] */
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
