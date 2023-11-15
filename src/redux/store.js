f32536ecebded62ddeb0b8c26a7371b62fa380a4
binyamT <binyampowerhc@gmail.com>
2023-11-15 03:10:40 +0300

    create a slice for fetching detail car info and update the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+import carReducer from './carSlice';
+import detailCarReducer from './detailCarSlice';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    car: carReducer,
+    detailCarInfo: detailCarReducer,
   },
 });
 
